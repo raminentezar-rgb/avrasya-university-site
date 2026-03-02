1ef68b57d6bc74c03e9354d8ff598f7e162d9ffe
raminentezar-rgb <raminentezarprogrammer@gmail.com>
2026-03-02 11:12:37 +0300

    Yeni sayfalar eklendi: [örneğin accounts ve aday_ogrenci klasörleri güncellendi]

--- otomotiv/admin.py
+++ otomotiv/admin.py
@@ -1,3 +1,55 @@
-from django.contrib import admin
+# app_name: otomotiv/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import OtomotivDuyuru, OtomotivEtkinlik, OtomotivDersProgrami
+
+
+@admin.register(OtomotivEtkinlik)
+class OtomotivEtkinlikAdmin(admin.ModelAdmin):
+    list_display = ['baslik', 'etkinlik_turu', 'baslangic_tarihi', 'yer', 'yayinda']
+    list_filter = ['etkinlik_turu', 'yayinda', 'baslangic_tarihi', 'kayit_gerekiyor']
+    search_fields = ['baslik', 'yer', 'kisa_aciklama']
+    prepopulated_fields = {'slug': ['baslik']}
+    date_hierarchy = 'baslangic_tarihi'
+    
+    fieldsets = (
+        ('Temel Bilgiler', {
+            'fields': ('baslik', 'slug', 'etkinlik_turu', 'kisa_aciklama', 'detayli_aciklama')
+        }),
+        ('Tarih ve Yer', {
+            'fields': ('baslangic_tarihi', 'bitis_tarihi', 'yer')
+        }),
+        ('Görsel ve Katılım', {
+            'fields': ('afis', 'katilim_linki', 'kayit_gerekiyor', 'ucretli')
+        }),
+        ('Yayın Ayarları', {
+            'fields': ('yayinda',)
+        }),
+    )
+
+
+@admin.register(OtomotivDuyuru)
+class OtomotivDuyuruAdmin(admin.ModelAdmin):
+    list_display = ['baslik', 'fakulte', 'yayin_tarihi', 'yayinda']
+    list_filter = ['yayinda', 'yayin_tarihi']
+    search_fields = ['baslik', 'icerik']
+    
+    def get_queryset(self, request):
+        return OtomotivDuyuru.objects.all()
+
+
+@admin.register(OtomotivDersProgrami)
+class OtomotivDersProgramiAdmin(admin.ModelAdmin):
+    list_display = ['baslik', 'sinif', 'yayin_tarihi', 'aktif']
+    list_filter = ['sinif', 'aktif', 'yayin_tarihi']
+    search_fields = ['baslik', 'aciklama']
+    list_editable = ['aktif']
+    
+    fieldsets = (
+        ('Temel Bilgiler', {
+            'fields': ('baslik', 'aciklama', 'dosya')
+        }),
+        ('Ayarlar', {
+            'fields': ('sinif', 'aktif')
+        }),
+    )
--- otomotiv/apps.py
+++ otomotiv/apps.py
@@ -1,5 +1,9 @@
+# app_name: otomotiv/apps.py
+
 from django.apps import AppConfig
 
 
 class OtomotivConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'otomotiv'
+    verbose_name = 'Otomotiv'
