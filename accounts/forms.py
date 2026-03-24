fe5da5b0edf7f194d4439ea1f9c08f661a0239c1
raminentezar-rgb <raminentezarprogrammer@gmail.com>
2026-03-24 16:03:54 +0300

    Yeni sayfalar eklendi: [örneğin accounts ve aday_ogrenci klasörleri güncellendi]

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 class StudentSignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    full_name = forms.CharField(max_length=150)
+    first_name = forms.CharField(max_length=150, required=True)
+    last_name = forms.CharField(max_length=150, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'full_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email')
