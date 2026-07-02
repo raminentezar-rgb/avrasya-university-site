39d51baf644f4b2b5747474993cc82f4cb192b32
raminentezar-rgb <raminentezarprogrammer@gmail.com>
2026-07-02 13:36:11 +0300

    Yeni sayfalar eklendi: [örneğin accounts ve aday_ogrenci klasörleri güncellendi]

---
+++ yonetim_bilisim_sistemleri/__init__.py
@@ -0,0 +1,3 @@
+# app_name: yonetim_bilisim_sistemleri/__init__.py
+
+default_app_config = 'yonetim_bilisim_sistemleri.apps.YonetimBilisimSistemleriConfig'
--- yonetim_bilisim_sistemleri/apps.py
+++ yonetim_bilisim_sistemleri/apps.py
@@ -1,5 +1,9 @@
+# app_name: yonetim_bilisim_sistemleri/apps.py
+
 from django.apps import AppConfig
 
 
 class YonetimBilisimSistemleriConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'yonetim_bilisim_sistemleri'
+    verbose_name = 'Yönetim Bilişim Sistemleri'
